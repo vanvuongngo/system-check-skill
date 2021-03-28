5b8f22abb8fcb76cd2bacbc5ca4655e9cf0a27b8
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2021-03-28 17:05:07 +0200

    add check green and blue

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2021 Van Vuong Ngo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from mycroft import MycroftSkill, intent_file_handler
 from smbus2 import SMBus
 import time
@@ -28,6 +36,38 @@ class SystemCheck(MycroftSkill):
             self.speak_dialog('color_checked', data={"color": "red"})
         elif response == 'no':
             self.speak_dialog('color_invalid', data={"color": "red"})
+
+        # green
+        pixels = []
+        for i in range(6):
+            pixels.append(0)
+            pixels.append(255)
+            pixels.append(0)
+        bus.write_i2c_block_data(address, 0, pixels)
+        bus.write_i2c_block_data(address, 6, pixels)
+
+        time.sleep(2)
+        response = self.ask_yesno('check_colors', data={"color": "green"})
+        if response == 'yes':
+            self.speak_dialog('color_checked', data={"color": "green"})
+        elif response == 'no':
+            self.speak_dialog('color_invalid', data={"color": "green"})
+
+        # blue
+        pixels = []
+        for i in range(6):
+            pixels.append(0)
+            pixels.append(0)
+            pixels.append(255)
+        bus.write_i2c_block_data(address, 0, pixels)
+        bus.write_i2c_block_data(address, 6, pixels)
+
+        time.sleep(2)
+        response = self.ask_yesno('check_colors', data={"color": "blue"})
+        if response == 'yes':
+            self.speak_dialog('color_checked', data={"color": "blue"})
+        elif response == 'no':
+            self.speak_dialog('color_invalid', data={"color": "blue"})
 
         bus.close()
 
